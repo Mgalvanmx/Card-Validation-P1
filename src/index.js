bab0cb54ba1878e6014fe23868bc5f2dedca0971
Mgalvanmx <rgalvangtz@gmail.com>
2022-10-11 12:34:14 -0500

    Agrega boton validar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+
+
+const cardNumber = document.getElementById("cardNumber")
+const botonValidar = document.getElementById("botonValidar")
+
+botonValidar.addEventListener("click", function(){
+if (cardNumber.value.length <16){
+  alert ("Por favor ingresa la numeración de tu tarjeta para continuar");
+}else {
+    let validation = validator.isValid(cardNumber.value)
+    let maskedNumber = validator.maskify(cardNumber.value)
+    cardNumber.value = maskedNumber
+    if (validation){
+      alert ("Tarjeta Valida")
+      } else {
+        alert ("Tarjeta Invalida")
+      }
+} })
+
+
+
+ 
