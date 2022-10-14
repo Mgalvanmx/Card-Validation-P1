87a6128c784088d84ded438cf1285e1aac1e36d4
Mgalvanmx <rgalvangtz@gmail.com>
2022-10-14 01:40:53 -0500

    ADD ID  INFO Y LOGOS

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import validator from './validator.js';
 
 const cardNumber = document.getElementById("cardNumber")
 const botonValidar = document.getElementById("botonValidar")
+const digitos = document.querySelector("#tarjeta .digitos")
+const nombreth = document.querySelector("#tarjeta .nombreth")
+const logoMarca = document.getElementById("logo-marca")
 
 botonValidar.addEventListener("click", function(){
 if (cardNumber.value.length <16){
@@ -19,6 +22,50 @@ if (cardNumber.value.length <16){
         alert ("Tarjeta Invalida")
       }
 } })
+
+//* PASAR NÚMERO DE TARJETA
+
+cardNumber.addEventListener('keyup', (e) => {
+  let numeracion = e.target.value;
+
+  cardNumber.value = numeracion 
+//* ELIMINAR ESPACIOS EN BLANCO
+.replace(/\s/g, '')
+//* AGRUPAR NÚMEROS Y COLOCAR ESPACIO
+.replace(/([0-9]{4})/g, '$1 ')
+//* ELIMINA ULTIMO ESPACIO
+.trim();
+
+digitos.textContent = numeracion;
+
+if(numeracion == ""){
+  digitos.textContent = "0000 0000 0000 0000";
+//* QUITAR EL LOGO DE LA TARJETA AL BORRAR NUM
+logoMarca.innerHTML ="";  
+}
+//* RECONOCER VISA o MC
+if(numeracion[0] == 4){
+//* NO SE REPITA LOGO  
+  logoMarca.innerHTML = "";
+  const imagen = document.createElement("img");
+  imagen.src = "Imagenes/VISA.png";
+  logoMarca.appendChild(imagen);
+} else if(numeracion[0] == 5){
+  logoMarca.innerHTML = "";
+  const imagen = document.createElement("img");
+  imagen.src = "Imagenes/MC.png";
+  logoMarca.appendChild(imagen);
+} 
+
+});
+
+//* PASAR NOMBRE A TARJETA
+nombreth.addEventListener('keyup', (e) => {
+  let name = e.target.value;
+
+nombreth.textContent = name;
+ 
+});
 
 
 
